38037f5a0b6452a1cb0da4ced158fa6a4e957fc2
Arthur Silva <arthurmatsilva@outlook.com>
2021-01-06 16:45:21 -0300

    Click and hold to draw and erase button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let color = 'black';
+let isDrawing = false;
 
 function generateGrid(size) {
   const grid = document.querySelector('#grid');
@@ -11,14 +12,18 @@ function generateGrid(size) {
     for (let j = 0; j < size; j++) {
       const div = document.createElement('div');
 
-      div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = color;
-        e.target.style.borderColor = color;
-      });
+      div.addEventListener('mousemove', draw);
 
       grid.appendChild(div);
     }
   }
+}
+
+function draw() {
+  if (!isDrawing) return;
+
+  this.style.backgroundColor = color;
+  this.style.borderColor = color;
 }
 
 function clearGrid() {
@@ -48,5 +53,13 @@ clearBtn.addEventListener('click', clearGrid);
 
 const colorBtn = document.querySelector('#color');
 colorBtn.addEventListener('change', changeColor);
+colorBtn.addEventListener('click', changeColor);
+
+const eraseBtn = document.querySelector('#erase');
+eraseBtn.addEventListener('click', () => color = '#FFF');
+
+const grid = document.querySelector('#grid');
+grid.addEventListener('mousedown', () => isDrawing = true);
+grid.addEventListener('mouseup', () => isDrawing = false);
 
 generateGrid(16);
