eb52763b839b7f3fe059062bbc8740672486c011
Arthur Silva <arthurmatsilva@outlook.com>
2022-02-11 14:09:18 -0300

    refactor: changing the code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-let color = 'black';
+let color = "#000";
 let isDrawing = false;
 
 function generateGrid(size) {
-  const grid = document.querySelector('#grid');
-
+  const grid = document.querySelector("#grid");
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.innerHTML = "";
 
-  grid.innerHTML = '';
-
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++)
     for (let j = 0; j < size; j++) {
-      const div = document.createElement('div');
+      const cell = document.createElement("div");
+      cell.addEventListener("mousemove", draw);
 
-      div.addEventListener('mousemove', draw);
-
-      grid.appendChild(div);
+      grid.appendChild(cell);
     }
-  }
 }
 
 function draw() {
@@ -27,16 +23,15 @@ function draw() {
 }
 
 function clearGrid() {
-  const grid = document.querySelectorAll('#grid div');
-
-  grid.forEach(div => {
-    div.style.backgroundColor = '#FFF'
-    div.style.borderColor = 'rgb(243, 243, 243)';
+  const grid = document.querySelectorAll("#grid div");
+  grid.forEach(cell => {
+    cell.style.backgroundColor = "#FFF";
+    cell.style.borderColor = "#f3f3f3";
   });
 }
 
 function newGrid() {
-  const size = window.prompt('New grid size');
+  const size = window.prompt("New grid size");
 
   if (size >= 2 && size <= 100) generateGrid(size);
 }
@@ -45,27 +40,27 @@ function changeColor(event) {
   color = event.target.value;
 }
 
-const newBtn = document.querySelector('#new');
-newBtn.addEventListener('click', newGrid);
+const newBtn = document.querySelector("#new");
+newBtn.addEventListener("click", newGrid);
 
-const clearBtn = document.querySelector('#clear');
-clearBtn.addEventListener('click', clearGrid);
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", clearGrid);
 
-const colorBtn = document.querySelector('#color');
-colorBtn.addEventListener('change', event => {
+const colorBtn = document.querySelector("#color");
+colorBtn.addEventListener("change", event => {
   changeColor(event);
   colorBtn.style.backgroundColor = color;
 });
-colorBtn.addEventListener('click', event => {
+colorBtn.addEventListener("click", event => {
   changeColor(event);
   colorBtn.style.backgroundColor = color;
 });
 
-const eraseBtn = document.querySelector('#eraser');
-eraseBtn.addEventListener('click', () => color = '#FFF');
+const eraseBtn = document.querySelector("#eraser");
+eraseBtn.addEventListener("click", () => color = "#FFF");
 
-const grid = document.querySelector('#grid');
-grid.addEventListener('mousedown', () => isDrawing = true);
-grid.addEventListener('mouseup', () => isDrawing = false);
+const grid = document.querySelector("#grid");
+grid.addEventListener("mousedown", () => isDrawing = true);
+grid.addEventListener("mouseup", () => isDrawing = false);
 
 generateGrid(16);
