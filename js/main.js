b21f24ddb6ee494a3350bca3a28950132ed9452f
Arthur Silva <arthurmatsilva@outlook.com>
2023-08-23 15:59:23 -0300

    style: updating de style of the code

--- js/main.js
+++ js/main.js
@@ -19,25 +19,26 @@ function draw() {
   if (!isDrawing) return;
 
   this.style.backgroundColor = color;
-  this.style.borderColor = color;
+  this.style.borderColor = color === "#fff" ? "#f1f1f1" : color;
 }
 
 function clearGrid() {
   const grid = document.querySelectorAll("#grid div");
-  grid.forEach(cell => {
-    cell.style.backgroundColor = "#FFF";
-    cell.style.borderColor = "#f3f3f3";
+  grid.forEach((cell) => {
+    cell.style.backgroundColor = "#fff";
+    cell.style.borderColor = "#f1f1f1";
   });
 }
 
 function newGrid() {
   const size = window.prompt("New grid size");
 
-  if (size >= 2 && size <= 100) generateGrid(size);
+  if (size >= 2 && size <= 128) generateGrid(size);
 }
 
-function changeColor(event) {
+function changeColor(event, element) {
   color = event.target.value;
+  element.style.backgroundColor = color;
 }
 
 const newBtn = document.querySelector("#new");
@@ -47,20 +48,14 @@ const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", clearGrid);
 
 const colorBtn = document.querySelector("#color");
-colorBtn.addEventListener("change", event => {
-  changeColor(event);
-  colorBtn.style.backgroundColor = color;
-});
-colorBtn.addEventListener("click", event => {
-  changeColor(event);
-  colorBtn.style.backgroundColor = color;
-});
+colorBtn.addEventListener("change", (e) => changeColor(e, colorBtn));
+colorBtn.addEventListener("click", (e) => changeColor(e, colorBtn));
 
 const eraseBtn = document.querySelector("#eraser");
-eraseBtn.addEventListener("click", () => color = "#FFF");
+eraseBtn.addEventListener("click", () => (color = "#fff"));
 
 const grid = document.querySelector("#grid");
-grid.addEventListener("mousedown", () => isDrawing = true);
-grid.addEventListener("mouseup", () => isDrawing = false);
+grid.addEventListener("mousedown", () => (isDrawing = true));
+grid.addEventListener("mouseup", () => (isDrawing = false));
 
 generateGrid(16);
